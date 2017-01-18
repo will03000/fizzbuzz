8ec498ca113b88d8ee69c0d9d6bedc4a2521878e
William <william.legal03@gmail.com>
2017-01-18 21:29:21 +0100

    good

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function fizzBuzz(number){
+if(number==2) {
+	console.log("2");
+}else if(number ==3){
+	console.log("fizz");
+}else{
+	console.log("echec"); 
+}
+//si mon chiffre egale à 3 
+//message fizz
+
+
+//si chiffre egal 5
+//ecrit buzz
+//ou le nombre qui est ecrit
+else if (number==5) {
+	console.log("buzz");
+}else(number==30){
+	console.log("fizzbuzz");
+}
+{
+	console.log("echec");
+}
+}
+fizzBuzz(50);
